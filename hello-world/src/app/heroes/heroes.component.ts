9b47efda575b555c9e58bf2f00fd4c8d539d8920
Mariana Fujikawa <mari.fujikawa97@gmail.com>
2021-08-09 15:27:37 -0300

    get data from a server + Create, Update, Delete + error handling + search

--- hello-world/src/app/heroes/heroes.component.ts
+++ hello-world/src/app/heroes/heroes.component.ts
@@ -22,7 +22,6 @@ export class HeroesComponent implements OnInit {
   ) {}
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
-
   }
   ngOnInit(): void {
     this.getHeroes();
@@ -30,5 +29,18 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`olha só id=${hero.id}`);
+  }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h) => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 }
